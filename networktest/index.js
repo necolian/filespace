b94f876fd36973d20b27d627ad4619e32f4132f1
ネコリアン <153508475+necolian@users.noreply.github.com>
2024-04-10 09:16:16 +0900

    Create index.js

---
+++ networktest/index.js
@@ -0,0 +1,18 @@
+//getリクエスト
+const post = new XMLHttpRequest();
+post.open(GET,"https://httbin.org");
+post.send();
+
+post.onreadystatechange = function() {
+
+	if(post.readystate === 4 && post.status === 200){
+  
+  	alert( post.responseText );
+  
+	}else{
+
+	alert("error   status = " + post.status );
+
+	}
+ 
+}
